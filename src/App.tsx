9ad2dbcaee7affa8ddd66008daeb7e57c22b503a
e44alex <e44alex@gmail.com>
2023-03-04 11:16:18 +0300

    added react-i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
-import {CatFact} from "./compoents/CatFact";
+import { CatFact } from "./compoents/CatFact";
+import { translationKeys } from "./translationKeys";
+import { useTranslation } from "react-i18next";
 
 export default function App() {
+  const {t} = useTranslation();
+
   return (
     <div className="App">
-      <CatFact />
+      <h1>
+        {t(translationKeys.app.welcome)}
+      </h1>
+      <CatFact/>
     </div>
   )
 }
---
+++ src/translationKeys.ts
@@ -0,0 +1,5 @@
+export const translationKeys = {
+  app: {
+    welcome: "Welcome"
+  }
+}
